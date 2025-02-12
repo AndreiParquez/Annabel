e00d3573d86181be247fca91525a044e693a5344
Andrei <jackparquez1@gmail.com>
2025-02-12 09:38:32 +0800

    up

--- src/components/CustomMap.jsx
+++ src/components/CustomMap.jsx
@@ -1,18 +1,31 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix for missing marker
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const customIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41], // Default size
+  iconAnchor: [12, 41], // Position relative to the point
+  popupAnchor: [1, -34],
+});
 
 const CustomMap = () => {
-  const position = [14.5995, 120.9842]; // Manila, Philippines (Change to your location)
+  const position = [14.2486504,121.1259126]; // Manila, Philippines (Change to your location)
 
   return (
-    <MapContainer center={position} zoom={15} className="leaflet-container">
+    <MapContainer center={position} zoom={60} className="leaflet-container">
 <TileLayer
   url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
 />
 
 
       {/* Marker for your location */}
-      <Marker position={position}>
+      <Marker position={position} icon={customIcon}>
         <Popup>📍 Annabell's Closet - Visit us here!</Popup>
       </Marker>
     </MapContainer>
